9ad1c8cafd97308fc068f70c949e23cd76bd3df1
Xanner <arek9513@gmail.com>
2023-06-02 15:44:29 +0200

    Init

---
+++ src/MySpot.Api/Controllers/ReservationsController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySpot.Api.Entities;
+using MySpot.Api.Services;
+
+namespace MySpot.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ReservationsController : ControllerBase
+{
+    private readonly ReservationsService _service = new();
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Reservation>> Get() => Ok(_service.GetAll);
+
+    [HttpGet("{id:guid}")]
+    public ActionResult<Reservation> Get(Guid id)
+    {
+        var reservation = _service.Get(id);
+        if (reservation is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(reservation);
+    }
+
+    [HttpPost]
+    public ActionResult Post(Reservation reservation)
+    {
+        var id = _service.Create(reservation);
+        if (id is null)
+        {
+            return BadRequest();
+        }
+
+        return CreatedAtAction(nameof(Get), new { id }, null);
+    }
+
+    [HttpPut("{id:guid}")]
+    public ActionResult Put(Guid id, Reservation reservation)
+    {
+        if(_service.Update(id, reservation))
+        {
+            return NoContent();
+        }
+
+        return NotFound();
+    }
+
+    [HttpDelete("{id:guid}")]
+    public ActionResult Delete(Guid id)
+    {
+        if (_service.Delete(id))
+        {
+            return NoContent();
+        }
+
+        return NotFound();
+    }
+}
---
+++ src/MySpot.Api/Services/ReservationsService.cs
@@ -0,0 +1,66 @@
+﻿using MySpot.Api.Entities;
+
+namespace MySpot.Api.Services;
+
+public class ReservationsService
+{
+    private static List<WeeklyParkingSpot> WeeklyParkingSpots = new()
+    {
+        new WeeklyParkingSpot(Guid.NewGuid(), DateTime.UtcNow, DateTime.UtcNow.AddDays(7), "P1"),
+        new WeeklyParkingSpot(Guid.NewGuid(), DateTime.UtcNow, DateTime.UtcNow.AddDays(7), "P2"),
+        new WeeklyParkingSpot(Guid.NewGuid(), DateTime.UtcNow, DateTime.UtcNow.AddDays(7), "P3"),
+        new WeeklyParkingSpot(Guid.NewGuid(), DateTime.UtcNow, DateTime.UtcNow.AddDays(7), "P4"),
+        new WeeklyParkingSpot(Guid.NewGuid(), DateTime.UtcNow, DateTime.UtcNow.AddDays(7), "P5")
+    };
+
+    public Reservation Get(Guid id)
+        => GetAllWeekly().SingleOrDefault(x => x.Id == id);
+
+    public IEnumerable<Reservation> GetAllWeekly() 
+        => WeeklyParkingSpots.SelectMany(x => x.Reservations);
+
+    public Guid? Create(Reservation reservation)
+    {
+        var weeklyParkingSpot = WeeklyParkingSpots.SingleOrDefault(x => x.Id == reservation.ParkingSpotId);
+        if(weeklyParkingSpot is null)
+        {
+            return default;
+        }
+
+        reservation.Id = Guid.NewGuid();
+        weeklyParkingSpot.AddReservation(reservation);
+
+        return reservation.Id;
+    }
+
+    public bool Update(int id, Reservation reservation)
+    {
+        var existingReservation = Reservations.SingleOrDefault(x => x.Id == id);
+        if (existingReservation is null)
+        {
+            return false;
+        }
+
+        if(existingReservation.Date < DateTime.UtcNow)
+        {
+            return false;
+        }
+
+        existingReservation.LicensePlate = reservation.LicensePlate;
+
+        return true;
+    }
+
+    public bool Delete(int id)
+    {
+        var existingReservation = Reservations.SingleOrDefault(x => x.Id == id);
+        if (existingReservation is null)
+        {
+            return false;
+        }
+
+        Reservations.Remove(existingReservation);
+
+        return true;
+    }
+}
