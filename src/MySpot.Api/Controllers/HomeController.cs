4fde48b989263371aa426a0f0c2bdafdc7e53730
Arkadiusz Kliś <arek9513@gmail.com>
2023-06-15 15:17:31 +0200

    Add app configuration

---
+++ src/MySpot.Api/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using MySpot.Infrastructure;
+
+namespace MySpot.Api.Controllers;
+
+[Route("")]
+public class HomeController : ControllerBase
+{
+    private readonly string _name;
+
+    public HomeController(IOptions<AppOptions> appOptions)
+    {
+        _name = appOptions.Value.Name;
+    }
+
+    [HttpGet]
+    public ActionResult<string> Get() => _name;
+}
